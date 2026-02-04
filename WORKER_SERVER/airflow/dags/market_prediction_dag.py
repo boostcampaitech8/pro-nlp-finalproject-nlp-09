602d4ed72baf3671709cb8ed0a2850689982079a
ttotto0421 <ttotto0421@gmail.com>
2026-02-04 10:35:56 +0000

    refactor: airflow버전 변경

--- WORKER_SERVER/airflow/dags/market_prediction_dag.py
+++ WORKER_SERVER/airflow/dags/market_prediction_dag.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.providers.standard.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator
 from airflow.models import Variable
 from datetime import datetime, timedelta
 import sys
